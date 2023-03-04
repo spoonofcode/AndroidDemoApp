3ae8b10e15983d6855af996dcdbb47ab3cb46542
spoonofcode <102907783+spoonofcode@users.noreply.github.com>
2023-03-04 12:43:14 +0100

    #2 migrate gradle groovy dsl files to kotlin dsl files (#6)
    * #2 Migrate to gradle groovy dsl files to kotlin dsl files
    
    Add versions, dependecies and app configuration files and adopt to builds
    Update dependencies versions

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = AppConfig.namespace
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+    }
+
+    flavorDimensions(AppConfig.dimension)
+    productFlavors {
+        create("staging") {
+            applicationIdSuffix = ".staging"
+            dimension = AppConfig.dimension
+        }
+
+        create("production") {
+            dimension = AppConfig.dimension
+        }
+    }
+
+    packagingOptions {
+        resources {
+            exclude("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.composeCompiler
+    }
+
+}
+
+dependencies {
+    //std lib
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    //app libs
+    implementation(Dependencies.appLibraries)
+    //test libs
+    testImplementation(Dependencies.testLibraries)
+    androidTestImplementation(Dependencies.androidTestLibraries)
+
+}
