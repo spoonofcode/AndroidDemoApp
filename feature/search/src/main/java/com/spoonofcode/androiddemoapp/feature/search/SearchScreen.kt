01f98a0351c3a7aa7112d1e78ac97e4b57f8d1da
Luczak Bartosz <spoonofcode@gmail.com>
2023-03-29 19:39:01 +0200

    #10 Remove not need accompanist libraries

--- feature/search/src/main/java/com/spoonofcode/androiddemoapp/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/com/spoonofcode/androiddemoapp/feature/search/SearchScreen.kt
@@ -2,7 +2,6 @@ package com.spoonofcode.androiddemoapp.feature.search
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
